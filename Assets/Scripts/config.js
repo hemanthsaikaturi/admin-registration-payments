f575f493636694661de36fb2196dc14f8935901c
hemanth <hemanthsaikaturi@gmail.com>
2025-08-08 17:47:53 +0530

    fixed directory

--- Assets/Scripts/config.js
+++ Assets/Scripts/config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBYMp2-oF24Fd5oJZhVDJWVZW3iHigBo4Q",
-  authDomain: "registration-website-admin.firebaseapp.com",
-  projectId: "registration-website-admin",
-  storageBucket: "registration-website-admin.firebasestorage.app",
-  messagingSenderId: "88040719942",
-  appId: "1:88040719942:web:d5d2c2adff9740d7cdec47",
+  apiKey: "AIzaSyBD_DuHR-l-ejGyts8cGPRA83jjVqdjxJM",
+  authDomain: "testing-registration-admin.firebaseapp.com",
+  projectId: "testing-registration-admin",
+  storageBucket: "testing-registration-admin.firebasestorage.app",
+  messagingSenderId: "484911943555",
+  appId: "1:484911943555:web:3cbe36c5a8408675f270b6",
 };
