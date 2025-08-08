5dcd6bab59882b59d35f1c7d710671293152d70e
hemanth <hemanthsaikaturi@gmail.com>
2025-08-08 17:45:33 +0530

    configuration!

---
+++ admin-registration-main/Assets/Scripts/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBD_DuHR-l-ejGyts8cGPRA83jjVqdjxJM",
+  authDomain: "testing-registration-admin.firebaseapp.com",
+  projectId: "testing-registration-admin",
+  storageBucket: "testing-registration-admin.firebasestorage.app",
+  messagingSenderId: "484911943555",
+  appId: "1:484911943555:web:3cbe36c5a8408675f270b6",
+};
